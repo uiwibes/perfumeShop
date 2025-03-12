39e07739cc8386c35d0c1b3fb6cbbef50c2d10df
ViraCodingPlus <155279076+ViraCodingPlus@users.noreply.github.com>
2025-03-12 03:58:04 +0330

    Update githubLink.js

--- js/githubLink.js
+++ js/githubLink.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+    alert(window.location.hostname);
     if (window.location.hostname === "uiwibes.github.io") {
       document.querySelectorAll("a").forEach(link => {
         link.href = "/perfumeShop/" + link.getAttribute("href");
