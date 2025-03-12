4003df74f630096c1a4af538f4304ef63737c06a
ViraCodingPlus <viracodingplus@gmail.com>
2025-03-12 04:05:00 +0330

    Modify GitHub Pages link handling to use host instead of hostname

--- js/githubLink.js
+++ js/githubLink.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    alert(window.location.hostname);
-    if (window.location.hostname === "uiwibes.github.io") {
+    // alert(window.location.host);
+    if (window.location.host === "uiwibes.github.io") {
       document.querySelectorAll("a").forEach(link => {
         link.href = "/perfumeShop/" + link.getAttribute("href");
       });
